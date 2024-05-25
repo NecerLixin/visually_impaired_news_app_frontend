47aa73dbbe927662e392a17363fffa01d2682a9a
XiaoShiYangxsy <1740122621@qq.com>
2024-05-25 17:55:32 +0800

    添加新闻页面

---
+++ entry/src/main/ets/view/NewsViewPage.js
@@ -0,0 +1,31 @@
+"use strict";
+class Item {
+    constructor(name, image, info) {
+        this.name = name;
+        this.image = image;
+        this.info = info;
+    }
+}
+struct Header extends  {
+    constructor() { }
+    Row() {
+        ;
+        width(30)
+            .height(30)
+            .backgroundColor(Color.White)
+            .type(ButtonType.Normal);
+        Text(this.message);
+        ;
+        width(30)
+            .height(30)
+            .backgroundColor(Color.White)
+            .type(ButtonType.Normal);
+    }
+}
+struct NewsViewPage extends   {
+    constructor() { }
+    build() {
+        ;
+    }
+}
+//# sourceMappingURL=NewsViewPage.js.map
